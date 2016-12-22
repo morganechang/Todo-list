d0dc451749e29b1ee3e9684f36ffda88d58be8fe
Morgane Chang <morganechang@gmail.com>
2016-12-22 11:20:41 -0500

    Initial

---
+++ main.js
@@ -0,0 +1,33 @@
+var listItems = ["Buy Gifts", "Watch Love Actually", "Finish Harry Potter"];
+
+var ul = document.getElementById("list"); // this is the container for the ul,
+
+for (var i = 0; i < listItems.length; i++) {
+	var li = document.createElement("li");  //this is a REFERENCE
+	li.appendChild(document.createTextNode(listItems[i]));
+	ul.appendChild(li);
+}
+
+function addItem(){
+	var item = document.getElementById("todo-input").value;
+	listItems.push(item);
+	var li = document.createElement("li");	// this is creating an empty list item; ACTION
+	li.appendChild(document.createTextNode(item));	
+	ul.appendChild(li);
+	document.getElementById("todo-input").value = "";
+	// parent element.appendChild (child)
+
+}
+
+// after item is added, clear input field
+
+
+//create a new list item 
+//clear the input field to type in new value
+
+
+	
+// var babez = ["Morgane", "Emma","Erin"];
+// for(i = 0; i < babez.length; i++)
+// console(babez[i] + " is awesome!"
+
