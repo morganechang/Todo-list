1deeeb8c9a6f9fb9575cb33bc78c840cfa49d902
Morgane Chang <morganechang@gmail.com>
2017-01-04 11:30:51 -0500

    styling + prevent empty string

--- main.js
+++ main.js
@@ -16,8 +16,18 @@ function addItem(){
 	ul.appendChild(li);
 	document.getElementById("todo-input").value = "";
 	// parent element.appendChild (child)
+ 	
+    if (item == "") { //refers to variable "item" (ie: the input's ID) that is empty
+        alert("You need to type something, you busy bee!");
+        return false;
+    }
 
+	// debugger
 }
+
+// function validateForm() {
+   
+// }
 
 // after item is added, clear input field
 
